09829af1b23f5a00d05c4e06e09565459016e664
Gildo Junior <1gildojunior@gmail.com>
2026-03-22 16:37:05 -0300

    feat: Improve back button style

--- src/components/_UI/BackButton.tsx
+++ src/components/_UI/BackButton.tsx
@@ -7,7 +7,7 @@ export function BackButton() {
       href="/"
       className="w-10 h-10 rounded-full shadow-game flex items-center justify-center cursor-pointer bg-black transition-transform hover:scale-110"
     >
-      <ArrowFatLeftIcon weight="fill" className="w-8 h-8 text-primary" />
+      <ArrowFatLeftIcon weight="fill" className="w-6 h-6 text-primary" />
     </Link>
   );
 }
