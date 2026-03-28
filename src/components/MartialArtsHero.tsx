321817ad00714ba522b2e6719ddf9b4c1f34021f
Gildo Junior <1gildojunior@gmail.com>
2026-03-28 12:05:51 -0300

    feat: Create hero component

---
+++ src/components/MartialArtsHero.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/utils/cn";
+import { WinsBadge } from "./_UI/WinsBadge";
+
+export function MartialArtsHero() {
+  return (
+    <div
+      className={cn(
+        "flex flex-col items-center justify-center gap-1 p-4 text-center",
+        "w-full max-w-180 my-0 mx-auto p-4",
+        "bg-primary bg-[url('/assets/dragon-background-texture.svg')] bg-no-repeat bg-bottom-right bg-cover xl:bg-contain shadow-hero",
+        "relative before:absolute before:content-[''] before:inset-0 before:border-24 before:border-transparent before:[border-image:url('/assets/dragon-border.svg')_500_stretch] before:pointer-events-none before:z-1",
+      )}
+    >
+      <h1 className="font-display text-hero-title text-shadow-hero-title">
+        Guess Today&apos;s Dragon Ball Character!
+      </h1>
+      <span className="inline-flex items-baseline gap-2 font-bold text-hero-subtitle m-0 text-shadow-hero-subtitle">
+        <WinsBadge count={20} className="self-center" />
+        people have already guessed today&apos;s character!
+      </span>
+    </div>
+  );
+}
