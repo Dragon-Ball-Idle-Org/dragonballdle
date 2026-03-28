510f2fd0f7e4df2eb19fe91c24e72252467a0c43
Gildo Junior <1gildojunior@gmail.com>
2026-03-28 12:06:26 -0300

    Create MainContainer default

---
+++ src/components/_UI/MainContainer.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export function MainContainer({ children }: { children: ReactNode }) {
+  return (
+    <main className="flex-1 flex flex-col items-center justify-center gap-4 w-full max-w-360 min-h-dvh my-3 mx-auto p-3">
+      {children}
+    </main>
+  );
+}
