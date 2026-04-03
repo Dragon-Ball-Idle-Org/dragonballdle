392ccdbb816c82f8d37de2f68f0ec7d74228c96d
Gildo Junior <1gildojunior@gmail.com>
2026-04-03 12:11:10 -0300

    fix: comparison method incorrect

--- src/utils/guess.ts
+++ src/utils/guess.ts
@@ -1,73 +1,8 @@
-import { CharacterGuess, compareSaga } from "@/types/guess";
-import { GuessStatus } from "@/types/guess";
+import { CharacterGuess, ClassicCharacter } from "@/types/guess";
 
-function compareArrayField(
-  guessArr?: { slug: string }[] | null,
-  answerArr?: { slug: string }[] | null,
-): GuessStatus {
-  const guessSafe = guessArr ?? [];
-  const answerSafe = answerArr ?? [];
-
-  const isBothEmpty = guessSafe.length === 0 && answerSafe.length === 0;
-
-  const hasExactMatch =
-    guessSafe.length === answerSafe.length &&
-    guessSafe.every((g) => answerSafe.some((a) => a.slug === g.slug));
-
-  const hasPartialMatch = guessSafe.some((g) =>
-    answerSafe.some((a) => a.slug === g.slug),
-  );
-
-  if (isBothEmpty) return GuessStatus.CORRECT;
-  if (hasExactMatch) return GuessStatus.CORRECT;
-  if (hasPartialMatch) return GuessStatus.PARTIAL;
-  return GuessStatus.WRONG;
-}
-
-export function compareGuess(guess: CharacterGuess, answer: CharacterGuess) {
+export function compareGuess(guessed: CharacterGuess, daily: ClassicCharacter) {
   return {
-    id: guess.slug,
-
-    character: {
-      status:
-        guess.slug === answer.slug ? GuessStatus.CORRECT : GuessStatus.WRONG,
-    },
-
-    gender: {
-      status:
-        guess.gender.slug === answer.gender.slug
-          ? GuessStatus.CORRECT
-          : GuessStatus.WRONG,
-    },
-
-    race: {
-      status: compareArrayField(guess.races, answer.races),
-    },
-
-    affiliation: {
-      status: compareArrayField(guess.affiliations, answer.affiliations),
-    },
-
-    transformation: {
-      status:
-        guess.has_transformations === answer.has_transformations
-          ? GuessStatus.CORRECT
-          : GuessStatus.WRONG,
-    },
-
-    attribute: {
-      status: compareArrayField(guess.attributes, answer.attributes),
-    },
-
-    series: {
-      status:
-        guess.series.slug === answer.series.slug
-          ? GuessStatus.CORRECT
-          : GuessStatus.WRONG,
-    },
-
-    debut_saga: {
-      status: compareSaga(guess.debut_saga, answer.debut_saga),
-    },
+    correct: guessed.slug === daily.slug,
+    thumb_src: `${process.env.NEXT_PUBLIC_CDN_BASE_URL}${daily.thumb_path}`,
   };
 }
