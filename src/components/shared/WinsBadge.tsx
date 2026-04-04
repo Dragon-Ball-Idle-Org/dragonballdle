47d03c8b4a1c953cec25aea365dd4f7a62fd6d57
Gildo Junior <1gildojunior@gmail.com>
2026-04-04 11:07:41 -0300

    feat: integrate loading state in WinsBadge and update MartialArtsHero component

--- src/components/shared/WinsBadge.tsx
+++ src/components/shared/WinsBadge.tsx
@@ -2,24 +2,57 @@
 
 import { cn } from "@/utils/cn";
 import { motion } from "framer-motion";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, Suspense } from "react";
 
 type WinsBadgeProps = PropsWithChildren<{
   count: number;
+  isLoading: boolean;
   className?: string;
 }>;
 
-export function WinsBadge({ count, className }: WinsBadgeProps) {
+export function WinsBadge({
+  count,
+  isLoading = false,
+  className,
+}: WinsBadgeProps) {
   return (
-    <motion.span
-      className={cn(
-        "inline-block py-1 px-2 rounded-full",
-        "bg-linear-135 from-green-500 to-green-700",
-        "font-ui shadow-wins-badge text-shadow-wins-badge",
-        className,
-      )}
+    <Suspense
+      fallback={
+        <span
+          className={cn(
+            "inline-block py-1 px-2 rounded-full",
+            "bg-linear-135 from-green-500 to-green-700",
+            "font-ui shadow-wins-badge text-shadow-wins-badge",
+            "animate-pulse",
+          )}
+        >
+          ...
+        </span>
+      }
     >
-      {count}
-    </motion.span>
+      {isLoading ? (
+        <span
+          className={cn(
+            "inline-block py-1 px-2 rounded-full",
+            "bg-linear-135 from-green-500 to-green-700",
+            "font-ui shadow-wins-badge text-shadow-wins-badge",
+            "animate-pulse",
+          )}
+        >
+          ...
+        </span>
+      ) : (
+        <motion.span
+          className={cn(
+            "inline-block py-1 px-2 rounded-full",
+            "bg-linear-135 from-green-500 to-green-700",
+            "font-ui shadow-wins-badge text-shadow-wins-badge",
+            className,
+          )}
+        >
+          {count}
+        </motion.span>
+      )}
+    </Suspense>
   );
 }
--- src/components/themed/MartialArtsHero.tsx
+++ src/components/themed/MartialArtsHero.tsx
@@ -5,17 +5,20 @@ import { WinsBadge } from "../shared/WinsBadge";
 import { getWinsCount } from "@/service/wins";
 import { useGameContext } from "@/contexts/GameContext";
 import { useTranslations } from "@/contexts/TranslationContext";
-import { useEffect } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export function MartialArtsHero() {
   const translations = useTranslations("hero");
   const { winsCount, updateWinsCount } = useGameContext();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getWinsCount().then((count) => {
-      if (typeof window === "undefined") return;
-      updateWinsCount(count);
-    });
+    getWinsCount()
+      .then((count) => {
+        if (typeof window === "undefined") return;
+        updateWinsCount(count);
+      })
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
@@ -30,8 +33,17 @@ export function MartialArtsHero() {
       <h1 className="font-display text-hero-title text-shadow-hero-title">
         {translations.title}
       </h1>
-      <span className="inline-flex items-baseline sm:gap-2 font-bold text-hero-subtitle m-0 text-shadow-hero-subtitle">
-        <WinsBadge count={winsCount ?? 0} className="self-center" />
+      <span
+        className={cn(
+          "inline-flex sm:gap-2 font-bold text-hero-subtitle m-0 text-shadow-hero-subtitle",
+          isLoading ? "items-start" : "items-baseline",
+        )}
+      >
+        <WinsBadge
+          count={winsCount ?? 0}
+          isLoading={isLoading}
+          className="self-center"
+        />
         {translations.subtitle}
       </span>
     </div>
