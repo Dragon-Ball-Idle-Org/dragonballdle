a7308578209dc683aee3055370def334b2fbd6d0
Gildo Junior <1gildojunior@gmail.com>
2026-04-21 19:48:32 -0300

    feat: Add sentry monitor

--- src/proxy.ts
+++ src/proxy.ts
@@ -30,5 +30,6 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+  // - … Sentry's tunnel route `/monitoring`
+  matcher: ["/((?!api|_next|_vercel|monitoring|.*\\..*).*)"],
 };
